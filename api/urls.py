6e9e94ec289b9e37454ec32130a9ce02f0b0a676
Peng_root <3347132783@qq.com>
2024-11-16 12:38:30 +0800

    设置 404 页面

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, re_path
 import api.views
 
-
 urlpatterns = [
     path('generate_handwritten_image/', api.views.generate_handwritten_image, name='generate_handwritten_image'),
     path('get_font_list/', api.views.get_font_list, name='get_font_list'),
